c5bc07f9b132f24583bc8328bc3862fa2d12d187
Mateus <mateuaraujo01@gmail.com>
2025-08-24 11:34:07 -0300

    initializing repo

---
+++ src/main/java/io/goods/bhgoods/BhgoodsApplication.java
@@ -0,0 +1,13 @@
+package io.goods.bhgoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhgoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhgoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/goods/bhgoods/BhgoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.goods.bhgoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhgoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
