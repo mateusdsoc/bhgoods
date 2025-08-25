663fbdf6328c35b1888b213491b010b35a4ef957
Mateus <mateuaraujo01@gmail.com>
2025-08-25 13:09:57 -0300

    mudanças em user e admin

--- src/main/java/io/goods/bhgoods/model/Admin.java
+++ src/main/java/io/goods/bhgoods/model/Admin.java
@@ -1,10 +1,24 @@
- package io.goods.bhgoods.model;
+package io.goods.bhgoods.model;
 
+import io.goods.bhgoods.enums.UserRole;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "admin")
-public class Admin extends User{
+public class Admin extends User {
     
+    public Admin() {}
+    
+    public Admin(String email, String senha) {
+        super(email, senha, UserRole.ADMIN);
+    }
+    
+    // @PrePersist ensures that even if role is not set, it will be ADMIN before saving to database
+    // This is a safety mechanism in case someone creates Admin using default constructor
+    @PrePersist
+    private void setRole() {
+        super.setRole(UserRole.ADMIN);
+    }
 }
--- src/main/java/io/goods/bhgoods/model/User.java
+++ src/main/java/io/goods/bhgoods/model/User.java
@@ -1,7 +1,10 @@
 package io.goods.bhgoods.model;
 
+import io.goods.bhgoods.enums.UserRole;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,4 +24,44 @@ public abstract class User {
 
     @Column(nullable = false)
     private String senha;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+    
+    protected User() {}
+    
+    protected User(String email, String senha, UserRole role) {
+        this.email = email;
+        this.senha = senha;
+        this.role = role;
+    }
+    
+    public Long getId() { 
+        return id; 
+    }
+    
+    public String getEmail() { 
+        return email; 
+    }
+    
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+    
+    public String getSenha() { 
+        return senha; 
+    }
+    
+    public void setSenha(String senha) { 
+        this.senha = senha; 
+    }
+    
+    public UserRole getRole() { 
+        return role; 
+    }
+    
+    public void setRole(UserRole role) { 
+        this.role = role; 
+    }
 }
