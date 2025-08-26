872a6fc01b4be7abb0dd784432514336e978eb3c
Mateus <mateuaraujo01@gmail.com>
2025-08-26 17:20:22 -0300

    adicionando requisicoes para o admin

--- src/main/java/io/goods/bhgoods/controller/AdminController.java
+++ src/main/java/io/goods/bhgoods/controller/AdminController.java
@@ -1,17 +1,27 @@
 package io.goods.bhgoods.controller;
 
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.goods.bhgoods.enums.StatusAprovacao;
+import io.goods.bhgoods.model.Restaurante;
 import io.goods.bhgoods.model.User;
+import io.goods.bhgoods.service.RestauranteService;
+
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
+
+    RestauranteService restauranteService;
 
     @GetMapping("/dashboard")
     @PreAuthorize("hasRole('ADMIN')")
@@ -23,8 +33,25 @@ public class AdminController {
 
     @GetMapping("/restaurantes")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<String> getAllRestaurantes() {
-        // In a real application, this would return actual restaurant data
-        return ResponseEntity.ok("Lista de todos os restaurantes (Admin only)");
-    }
+    public ResponseEntity<List<Restaurante>> getRestaurantesAprovados(
+        @RequestParam(value = "status", required = false) StatusAprovacao status
+    ){
+        if(status.equals(StatusAprovacao.APROVADO)){
+            return ResponseEntity.ok(
+                restauranteService.getRestaurantesByStatus(StatusAprovacao.APROVADO));
+        }
+        else if(status.equals(StatusAprovacao.PENDENTE)){
+            return ResponseEntity.ok(
+                restauranteService.getRestaurantesByStatus(StatusAprovacao.PENDENTE));
+        }
+        else if(status.equals(StatusAprovacao.REJEITADO)){
+            return ResponseEntity.ok(
+                restauranteService.getRestaurantesByStatus(StatusAprovacao.REJEITADO));
+        }
+        return ResponseEntity.ok(
+            restauranteService.getAllRestaurantes());
+    } 
+    
+   
+    
 }
