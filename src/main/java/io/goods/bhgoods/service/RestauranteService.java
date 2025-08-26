e31703e8b6d62e9eb55d80be5cb546664be8f2b9
Mateus <mateuaraujo01@gmail.com>
2025-08-26 17:21:08 -0300

    criando service restaurante, terceirizando responsabilkidades

---
+++ src/main/java/io/goods/bhgoods/service/RestauranteService.java
@@ -0,0 +1,51 @@
+package io.goods.bhgoods.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.goods.bhgoods.enums.StatusAprovacao;
+import io.goods.bhgoods.exceptions.RestauranteNaoEncontradoException;
+import io.goods.bhgoods.model.Restaurante;
+import io.goods.bhgoods.repository.RestauranteRepository;
+
+@Service
+public class RestauranteService {
+
+    @Autowired
+    RestauranteRepository repository;
+
+    public List<Restaurante> getAllRestaurantes(){
+        return repository.findAll();
+    }
+
+    public List<Restaurante> getRestaurantesByStatus(StatusAprovacao status){
+        return repository.findByStatusAprovacao(status);
+    }
+
+    public List<Restaurante> getRestaurantesByNomeAndStatus(String nome, StatusAprovacao status){
+        return repository.findByNomeAndStatusAprovacao(nome, status);
+    }
+
+    public List<Restaurante> getRestaurantesByEnderecoAndStatus(String endereco, StatusAprovacao status){
+        return repository.findByNomeAndStatusAprovacao(endereco, status);
+    }
+
+    public List<Restaurante> getRestaurantesByNomeContainingAndStatus(String nome, StatusAprovacao status){
+        return repository.findByNomeContainingAndStatusAprovacao(nome, status);
+    }
+
+    public List<Restaurante> getRestaurantesByEnderecoContainingAndStatus(String endereco, StatusAprovacao status){
+        return repository.findByEnderecoContainingAndStatusAprovacao(endereco, status);
+    }
+
+    public Restaurante getRestauranteById(Long id){
+        return repository.findById(id).orElseThrow(() -> new RestauranteNaoEncontradoException());
+    }
+
+    public Restaurante getRestauranteByEmail(String email){
+        return repository.findByEmail(email).orElseThrow(() -> new RestauranteNaoEncontradoException());
+    }
+    
+}
