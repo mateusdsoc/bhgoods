0f9560eb9b8a6905bd9f88c7f29917c9484747bb
Mateus <mateuaraujo01@gmail.com>
2025-08-27 17:32:43 -0300

    adicionando métodos de adicionar itens em cardapio

--- src/main/java/io/goods/bhgoods/model/Cardapio.java
+++ src/main/java/io/goods/bhgoods/model/Cardapio.java
@@ -1,5 +1,6 @@
 package io.goods.bhgoods.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
@@ -19,11 +20,42 @@ public class Cardapio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @OneToMany(mappedBy = "cardapio", cascade = CascadeType.ALL)
-    List<ItemCardapio> itens;
+    // Relacionamento unidirecional: Cardapio -> ItemCardapio (FK em itemCardapio.cardapio_id)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "cardapio_id")
+    private List<ItemCardapio> itens = new ArrayList<>();
 
     @OneToOne
     @JoinColumn(name = "restaurante_id")
     private Restaurante restaurante;
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<ItemCardapio> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemCardapio> itens) {
+        this.itens.clear();
+        if (itens != null) {
+            itens.forEach(this::addItem);
+        }
+    }
+
+    public void addItem(ItemCardapio item) {
+        if (item != null) {
+            this.itens.add(item);
+        }
+    }
+
+    public Restaurante getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
 
 }
