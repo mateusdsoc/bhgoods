fe8a2fccfa42113c32949ce073d8c5a689c49d59
Mateus <mateuaraujo01@gmail.com>
2025-08-27 17:23:44 -0300

    adding getters and setters

--- src/main/java/io/goods/bhgoods/model/ItemCardapio.java
+++ src/main/java/io/goods/bhgoods/model/ItemCardapio.java
@@ -29,5 +29,39 @@ public class ItemCardapio {
     @JoinColumn(name = "cardapio_id")
     private Cardapio cardapio;
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Cardapio getCardapio() {
+        return cardapio;
+    }
+
+    public void setCardapio(Cardapio cardapio) {
+        this.cardapio = cardapio;
+    }
 }
