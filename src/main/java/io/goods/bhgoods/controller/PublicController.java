0167d1e19457d868fa2a2d48b69f410a0aa8de8f
Mateus <mateuaraujo01@gmail.com>
2025-08-27 14:14:04 -0300

    criando controller de requisições publicas com métodos atualizados

---
+++ src/main/java/io/goods/bhgoods/controller/PublicController.java
@@ -0,0 +1,32 @@
+package io.goods.bhgoods.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.goods.bhgoods.dto.Restaurante.ResponseRestaurante;
+import io.goods.bhgoods.enums.CategoriaRestaurante;
+import io.goods.bhgoods.service.RestauranteService;
+
+@RestController
+@RequestMapping("/api/public")
+public class PublicController {
+
+    @Autowired
+    private RestauranteService restauranteService;
+
+    @GetMapping("/restaurantes")
+    public ResponseEntity<List<ResponseRestaurante>> getRestaurantesPublicos(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "categorias", required = false) List<CategoriaRestaurante> categorias
+    ) {
+        List<ResponseRestaurante> restaurantes = restauranteService.buscarRestaurantesPublicos(nome, categorias);
+        return ResponseEntity.ok(restaurantes);
+    }
+    
+}
