958d35b863f4533d72f7abacbb66297f37299a43
Mateus <mateuaraujo01@gmail.com>
2025-08-28 14:39:25 -0300

    criando método de retorno de cada restaurante individualmente

--- src/main/java/io/goods/bhgoods/controller/PublicController.java
+++ src/main/java/io/goods/bhgoods/controller/PublicController.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.goods.bhgoods.dto.Restaurante.ResponseRestaurante;
+import io.goods.bhgoods.enums.StatusAprovacao;
+import io.goods.bhgoods.model.Restaurante;
 import io.goods.bhgoods.service.RestauranteService;
+import io.goods.bhgoods.util.RestauranteToResponse;
 
 @RestController
 @RequestMapping("/api/public")
@@ -27,5 +31,13 @@ public class PublicController {
         List<ResponseRestaurante> restaurantes = restauranteService.buscarRestaurantesPublicos(nome, categorias);
         return ResponseEntity.ok(restaurantes);
     }
-    
+
+   @GetMapping("/restaurantes/{id}")
+   public ResponseEntity<ResponseRestaurante> getRestauranteById(@PathVariable Long id) throws IllegalAccessException {
+       Restaurante restaurante = restauranteService.getRestauranteById(id);
+       if(restaurante.getStatusAprovacao() != StatusAprovacao.APROVADO){
+            throw new IllegalAccessException("Restaurante não disponível");
+        }
+        return ResponseEntity.ok(RestauranteToResponse.restauranteToResponse(restaurante));
+   }
 }
